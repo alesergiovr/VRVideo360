9a9b28bff6aaa127ab345eaf40b6805557389b60
alesergiovr <alexnico@hotmail.it>
2017-01-15 19:38:13 +0100

    ewfwefwef

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 /*$(document).ready(function(){
     video = document.getElementById("video1");
-    video.addEventListener("canplaythrough",startFirstVideo);
+    
     });*/
 $(document).ready(function(){
     var video = document.getElementById("video1");
-    var but = document.getElementById("play-button");
-    but.addEventListener("click", function(e){
-        $("#play-button").remove();
-        video.play();
-    }, false);
+    video.addEventListener("canplaythrough",startFirstVideo);
 });
 
 function startFirstVideo(){
     video.removeEventListener("canplaythrough",startFirstVideo);
-    video.addEventListener("click",provaVideo);
+    video.play();
 }
 
 function provaVideo(){
